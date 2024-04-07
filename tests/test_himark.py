44f91355307b1e0863286bd6375cb331debcd05d
rsoyding <rodolphe.soyding@leclanche.com>
2024-04-07 18:48:56 +0200

    Add test to check that the markers really work

--- tests/test_himark.py
+++ tests/test_himark.py
@@ -4,7 +4,7 @@ current_directory = Path(__file__).parent
 config_json = current_directory.joinpath('config.json')
 
 
-def test_json_filter(pytester):
+def test_json_filter1(pytester):
     """Make sure that pytest accepts our fixture."""
 
     # create a temporary pytest test module
@@ -28,7 +28,38 @@ def test_json_filter(pytester):
 
     # fnmatch_lines does an assertion internally
     result.stdout.fnmatch_lines([
-        '::test_marker2 PASSED*',
+        'test_json_filter.py::test_marker2 PASSED',
+    ])
+
+    # make sure that we get a '0' exit code for the testsuite
+    assert result.ret == 0
+
+
+def test_json_filter2(pytester):
+    """Make sure that pytest accepts our fixture."""
+
+    # create a temporary pytest test module
+    pytester.makepyfile("""
+        import pytest
+        
+        @pytest.mark.marker2
+        def test_marker1():
+            assert True
+            
+        @pytest.mark.marker1
+        def test_marker2():
+            assert True
+    """)
+
+    # run pytest with the following cmd args
+    result = pytester.runpytest(
+        f'--json={config_json}',
+        '-vvv'
+    )
+
+    # fnmatch_lines does an assertion internally
+    result.stdout.fnmatch_lines([
+        'test_json_filter.py::test_marker1 PASSED',
     ])
 
     # make sure that we get a '0' exit code for the testsuite
