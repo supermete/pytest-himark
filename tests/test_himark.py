4edf1ee4d5912a8bd2d465a3b305c2e908eaeb7a
rsoyding <rodolphe.soyding@leclanche.com>
2024-04-07 18:59:21 +0200

    Add test with another config.json where all markers are disabled

--- tests/test_himark.py
+++ tests/test_himark.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 current_directory = Path(__file__).parent
 config_json = current_directory.joinpath('config.json')
+config_disabled_json = current_directory.joinpath('config_disabled.json')
 
 
 def test_json_filter1(pytester):
@@ -33,9 +34,9 @@ def test_json_filter1(pytester):
     result.stdout.fnmatch_lines([
         '*::test_marker2 PASSED*',
     ])
-    result.stdout.no_fnmatch_line([
+    result.stdout.no_fnmatch_line(
         '*::test_marker1 PASSED*',
-    ])
+    )
 
     # make sure that we get a '0' exit code for the testsuite
     assert result.ret == 0
@@ -70,16 +71,16 @@ def test_json_filter2(pytester):
     result.stdout.fnmatch_lines([
         '*::test_marker1 PASSED*',
     ])
-    result.stdout.no_fnmatch_line([
-        '*::test_marker2 PASSED*',
-    ])
+    result.stdout.no_fnmatch_line(
+        '*::test_marker2 PASSED*'
+    )
 
     # make sure that we get a '0' exit code for the testsuite
     assert result.ret == 0
 
 
 def test_json_filter_none(pytester):
-    """Check that all tests are executed when no json is used or if all markers are disabled."""
+    """Check that all tests are executed when no json is used."""
 
     # create a temporary pytest test module
     pytester.makepyfile("""
@@ -99,6 +100,42 @@ def test_json_filter_none(pytester):
 
     # run pytest with the following cmd args
     result = pytester.runpytest(
+        '-vvv'
+    )
+
+    # fnmatch_lines does an assertion internally
+    result.stdout.fnmatch_lines([
+        '*::test_marker1 PASSED*',
+        '*::test_marker2 PASSED*',
+        '*::test_marker3 PASSED*',
+    ])
+
+    # make sure that we get a '0' exit code for the testsuite
+    assert result.ret == 0
+
+
+def test_json_filter_disabled(pytester):
+    """Check that all tests are executed when all markers are disabled."""
+
+    # create a temporary pytest test module
+    pytester.makepyfile("""
+        import pytest
+        
+        @pytest.mark.marker2
+        def test_marker1():
+            assert True
+            
+        @pytest.mark.marker1
+        def test_marker2():
+            assert True
+        
+        def test_marker3():
+            assert True
+    """)
+
+    # run pytest with the following cmd args
+    result = pytester.runpytest(
+        f'--json={config_disabled_json}',
         '-vvv'
     )
 
